fff0e692a6a6d40cc4bd009a5357d34948afd27e
hkgnp <ben@pngs.cc>
2022-06-19 19:20:47 +0800

    Fixed admin update page

--- routes/admin-update.js
+++ routes/admin-update.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const { Organisation } = require('../models');
+const { Organisation } = require("../models");
 
-const { checkIfSuperUser } = require('../middlewares');
+const { checkIfSuperUser } = require("../middlewares");
 
-router.get('/', checkIfSuperUser, async (req, res) => {
+router.get("/", checkIfSuperUser, async (req, res) => {
   let allOrgs = await Organisation.fetchAll();
 
-  res.render('admin-update', {
+  res.render("admin-update", {
     allOrgs: allOrgs.toJSON(),
   });
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const numberOfServices = process.env.NO_OF_SERVICES;
 
   let i = 1;
@@ -24,37 +24,21 @@ router.post('/', async (req, res) => {
       require: false,
     });
 
-    let rehabCentreData = {
-      female_capacity: req.body['female_capacity' + i.toString()],
-      female_pending: req.body['female_pending' + i.toString()],
-      female_available: req.body['female_available' + i.toString()],
-      male_capacity: req.body['male_capacity' + i.toString()],
-      male_pending: req.body['male_pending' + i.toString()],
-      male_available: req.body['male_available' + i.toString()],
-      special_prog_female_capacity:
-        req.body['special_prog_female_capacity' + i.toString()],
-      special_prog_female_pending:
-        req.body['special_prog_female_pending' + i.toString()],
-      special_prog_female_available:
-        req.body['special_prog_female_available' + i.toString()],
-      special_prog_male_capacity:
-        req.body['special_prog_male_capacity' + i.toString()],
-      special_prog_male_pending:
-        req.body['special_prog_male_pending' + i.toString()],
-      special_prog_male_available:
-        req.body['special_prog_male_available' + i.toString()],
-      special_remarks: req.body['special_remarks' + i.toString()],
+    let icsData = {
+      earliest_admission: req.body["earliest_admission"],
+      medifund_cases: req.body["medifund_cases" + i.toString()],
+      special_remarks: req.body["special_remarks" + i.toString()],
     };
 
-    serviceToUpdate.set(rehabCentreData);
-    serviceToUpdate.set('last_updated', new Date());
+    serviceToUpdate.set(icsData);
+    serviceToUpdate.set("last_updated", new Date());
     serviceToUpdate.save();
 
     i++;
   }
 
-  req.flash('success_messages', 'Rehab Centre details successfully updated!');
-  res.redirect('/');
+  req.flash("success_messages", "ICS details successfully updated!");
+  res.redirect("/");
 });
 
 module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const { Organisation } = require('../../models');
+const express = require("express");
+const { Organisation } = require("../../models");
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  let rehabCentreDetails = await Organisation.fetchAll();
+router.get("/", async (req, res) => {
+  let icsDetails = await Organisation.fetchAll();
 
-  res.send(rehabCentreDetails);
+  res.send(icsDetails);
 });
 
 module.exports = router;
